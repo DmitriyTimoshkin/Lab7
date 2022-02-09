cd34965d5c293fae403cc3d90597dc3399b27e1f
stron <strong.timoshkin@mail.ru>
2022-02-09 12:39:38 +0400

    Сделали гамма коррекцию

--- 1Lec.py
+++ 1Lec.py
@@ -1,5 +1,6 @@
 import argparse
 from skimage.io import imread, imsave
+from skimage import data, exposure, img_as_float
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--path', type=str)
 parser.add_argument('-r', '--result', type=str)
@@ -9,4 +10,5 @@ args = parser.parse_args()
 path = args.path
 img = imread(path)
 print('Image shape: ', img.shape)
-
+image = img_as_float(img)
+gamma_corrected = exposure.adjust_gamma(image, args.gammma, args.gain)
