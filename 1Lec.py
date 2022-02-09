8fb5dd29c7ff420754725e2f8f57c36aa988fc45
stron <strong.timoshkin@mail.ru>
2022-02-09 12:34:30 +0400

    Считали фото ещё раз)

--- 1Lec.py
+++ 1Lec.py
@@ -1,5 +1,12 @@
+import argparse
 from skimage.io import imread, imsave
-path = input('Введите путь:\n')
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--path', type=str)
+parser.add_argument('-r', '--result', type=str)
+parser.add_argument('-gm', '--gamma', type=float)
+parser.add_argument('-gn', '--gain', type=float)
+args = parser.parse_args()
+path = args.path
 img = imread(path)
 print('Image shape: ', img.shape)
 
